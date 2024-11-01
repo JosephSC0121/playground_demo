f91b2df917fbf4109a5d0642bcb6d87a4d62c406
JosephSC0121 <josephsilvacasas0121@gmail.com>
2024-11-01 00:37:02 -0500

    feat: new api url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://37.27.11.226:8080'
+const BASE_URL = 'https://api-playground.josephsilvacasas.com/'
 
 export const api = axios.create({
   baseURL: BASE_URL,
   timeout: 5000
 })
 
-const executor_URL = 'http://localhost:8000'
+const executor_URL = 'http://37.27.11.226:8000'
 
 export const executor = axios.create({
   baseURL: executor_URL,
