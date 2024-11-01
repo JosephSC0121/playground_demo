2a3cdb071dc53bc93992f2155673061330f46dee
JosephSC0121 <josephsilvacasas0121@gmail.com>
2024-11-01 15:28:37 -0500

    feat: better ui experience

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -39,8 +39,8 @@ export default function Chatbot() {
       </Box>
 
       {loading && (
-        <Box display="flex" justifyContent="center" alignItems="center" position="fixed" top={0} bottom={0} left={0} right={0} bgcolor="rgba(255, 255, 255, 0.7)" zIndex={10}>
-          <CircularProgress size={50} />
+        <Box display="flex" justifyContent="center" alignItems="center" position="fixed" top={0} bottom={0} left={0} right={0} zIndex={10}>
+        
         </Box>
       )}
 
